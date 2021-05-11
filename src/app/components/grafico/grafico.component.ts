1d5234b059c194ef933f93f9957fcf37741d419d
Eduardo <54048170+Eduardo-ops@users.noreply.github.com>
2021-05-10 21:41:39 -0300

    Finalizacao

---
+++ src/app/components/grafico/grafico.component.ts
@@ -0,0 +1,45 @@
+import { Component, Input, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'grafico',
+  templateUrl: './grafico.component.html',
+  styleUrls: ['./grafico.component.css']
+})
+export class GraficoComponent implements OnInit {
+  maxValue
+  graficoData
+
+  @Input()
+  dados
+
+  ngOnInit () {
+  
+    this.maxValue = 0
+    
+
+    this.dados.filter(item => item).forEach(item => {
+      this.maxValue =
+        item.recMes > this.maxValue ? item.recMes : this.maxValue
+      this.maxValue =
+        item.despMes > this.maxValue ? item.despMes : this.maxValue
+    })
+
+    this.graficoData = this.dados.filter(item => item).map(item => {
+      return {
+        barra1: (item.recMes / this.maxValue) * 100,
+        barra2: (item.despMes / this.maxValue) * 100,
+        label: item.label,
+        rec: item.recMes,
+        des: item.despMes
+      }
+    })
+  }
+
+  /* nullGraph(){
+    let barra1 = 0
+    let barra2 = 0
+    let label = ''
+    let rec = 0
+    let des = 0
+  } */
+}
---
+++ src/app/components/transitions/mov.ts
@@ -0,0 +1,9 @@
+export class Mov {
+    tipo: string = ''
+    descricao: string = ''
+    data: string = ''
+    valor: number
+    receita: number = 0
+    despesa: number = 0
+    saldo: number = 0    
+}
